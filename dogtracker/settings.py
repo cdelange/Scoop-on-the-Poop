9998ca024d92b4dd06b4745b1f3a36f67ae5d48d
cdelange <casey.delange@gmail.com>
2019-08-18 21:18:44 -0500

    Updated settings

--- dogtracker/settings.py
+++ dogtracker/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '2_*zar1&ty0u=0kp_u(n2#nuz*o9o3r#6f_t7)ce5-h6y*9*su'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['thescoopsonthepoops.herokuapp.com']
 
 
 # Application definition
@@ -120,3 +120,4 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
